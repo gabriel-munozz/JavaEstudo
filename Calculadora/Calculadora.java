23a589d64e81f3ce4dc16c3f140dc72169301d43
Gabriel Muñoz <gabrielmachadomunoz@gmail.com>
2026-03-01 20:52:22 +0000

    calculadora em java

---
+++ Calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculadora{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        
+
+    }
+
+    public static float somaValores(){
+
+    }
+
+    public static float subtracaoValores(){
+        
+    }
+
+    public static float divisaoValores(){
+        
+    }
+
+    public static float multiplicacaoValores(){
+        
+    }
+
+} 
