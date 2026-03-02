99330be19dc9d484982fbb1eedddb833c846c2cf
Gabriel Muñoz <gabrielmachadomunoz@gmail.com>
2026-03-02 23:45:13 +0000

    Calculadora Operações

--- Calculadora/Calculadora.java
+++ Calculadora/Calculadora.java
@@ -8,8 +8,7 @@ public class Calculadora{
         int num1 = entrada.nextInt();
         System.out.print("Digite outro valor: ");
         int num2 = entrada.nextInt();
-        println();
-
+        System.out.println();
         int opcao = 0;
 
         do{
@@ -24,7 +23,23 @@ public class Calculadora{
 
             switch(opcao){
                 case 1:
-                    System.out.println("Resultado: " + Somar(num1, num2));
+                    System.out.println("Resultado: " + Somar(num1, num2)+"\n");
+                    break;
+                
+                case 2:
+                    System.out.println("Resultado: " + Subtracao(num1, num2)+"\n");
+                    break;
+
+                case 3: 
+                    System.out.println("Resultado: " + Multiplicacao(num1, num2)+"\n");
+                    break;
+
+                case 4: 
+                    if (num2 == 0){
+                        System.out.println("Não é possível dividir um número por 0\n");
+                    } else{
+                        System.out.println("Resultado: " + Divisao(num1, num2)+"\n");
+                    }
                     break;
 
                 case 5:
@@ -40,6 +55,24 @@ public class Calculadora{
         int soma;
         soma = num1 + num2;
         return soma;
+    }
+
+    public static int Subtracao(int num1, int num2){
+        int subtracao;
+        subtracao = num1 - num2;
+        return subtracao;
+    }
+
+    public static int Multiplicacao(int num1, int num2){
+        int multiplicacao;
+        multiplicacao = num1 * num2;
+        return multiplicacao;        
+    }
+
+    public static int Divisao(int num1, int num2){
+        int divisao;
+        divisao = num1 / num2;
+        return divisao;
     }
 
 } 
